db156cc5cf581e1c2564a136a933345cdcff8bb3
kakas <shrimptrain@gmail.com>
2016-11-16 15:59:59 +0800

    seller can add new product

--- app/controllers/seller/products_controller.rb
+++ app/controllers/seller/products_controller.rb
@@ -1,15 +1,33 @@
 class Seller::ProductsController < ApplicationController
-  before_action :set_shop
+  before_action :set_shop, only: [:index, :create]
   layout "seller"
 
   def index
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = @shop.products.build(product_params)
+
+    if @product.save
+      redirect_to seller_products_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_shop
     @shop = Shop.first
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :shop
+
+  validates :name, :price, :stock, presence: true
+  validates_numericality_of :price, :stock, greater_than_or_equal_to: 0
 end
